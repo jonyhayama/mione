b3fedf723e7316127dc4477bf19e27929aad26af
Jony Hayama <jonathan.hayama@gmail.com>
2020-08-11 08:40:51 -0300

    add vuetify

--- app/javascript/packs/vue.js
+++ app/javascript/packs/vue.js
@@ -6,10 +6,12 @@
 // All it does is render <div>Hello Vue</div> at the bottom of the page.
 
 import Vue from 'vue'
+import vuetify from '../src/plugins/vuetify'
 import App from '../app.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
+    vuetify,
     render: h => h(App)
   }).$mount()
   document.body.appendChild(app.$el)

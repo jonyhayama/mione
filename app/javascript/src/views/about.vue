9974023efae42c07c6f638507719bd5250ef6d8a
Jony Hayama <jonathan.hayama@gmail.com>
2020-08-14 16:10:51 -0300

    add about page content

--- app/javascript/src/views/about.vue
+++ app/javascript/src/views/about.vue
@@ -1,3 +1,13 @@
 <template>
-  <p>About</p>
+  <div>
+    <h2>Hi there!</h2>
+    <p>Hermione is an awesome sidekick! I'm no Harry Potter, but I wish she were my sidekick =p</p>
+    <p>With that in mind I've created this little app to help me whenever I need some magic :)</p>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+  div { 
+    text-align: center; 
+  }
+</style>
